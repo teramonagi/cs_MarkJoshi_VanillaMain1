7ecedcf79a1d707cb03f73bbe9b24d2d92448f3d
unknown <yana@.(none)>
2011-10-07 22:12:56 +0900

    First

---
+++ VanillaMain1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VanillaMain1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //parameters
+            double expiry = 1.0;
+            double spot = 49.0;
+            double vol = 0.2;
+            double r = 0.01;
+            double lower = 45.0;
+            double upper = 55.0;
+
+            //input number of montecarlo paths
+            ulong number_of_paths = 10000;
+            Console.Write("Enter number of montecarlo paths : ");
+            number_of_paths = ulong.Parse(Console.ReadLine());
+
+            //create payoff object
+            VanillaOption option = new VanillaOption(new PayOffDoubleDigital(lower, upper), expiry);
+
+            //montecarlo simulation & output result
+            double result = SimpleMC3.SimpleMonteCarlo3(option, spot, vol, r, number_of_paths);
+            Console.WriteLine("the price is " + result.ToString());
+        }
+    }
+}
---
+++ VanillaMain1/Vanilla1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VanillaMain1
+{
+    class VanillaOption
+    {
+        public VanillaOption(PayOff payoff_, double expiry_)
+        {
+            this.payoff = payoff_;
+            this.Expiry = expiry_;
+        }
+        public double PayOff(double spot_)
+        {
+            return payoff.Do(spot_);
+        }
+
+        public double Expiry{get; private set;}
+        private PayOff payoff;
+    }
+}
